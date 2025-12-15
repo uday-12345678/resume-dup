d24c41504f4977ace9cbdde9bce915478f9dfc57
Gemeni uday <uday.22bce8173@vitapstudent.ac.in>
2025-12-16 02:22:46 +0530

    Add files via upload

---
+++ learning_links.py
@@ -0,0 +1,462 @@
+LEARNING_LINKS = {
+
+    # ================= Programming & Core CS =================
+    "python": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/python-programming-language/"),
+            ("W3Schools", "https://www.w3schools.com/python/")
+        ],
+        "youtube": [
+            ("Bro Code", "https://www.youtube.com/@BroCodez"),
+            ("Corey Schafer", "https://www.youtube.com/@coreyms")
+        ]
+    },
+
+    "java": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/java/"),
+            ("W3Schools", "https://www.w3schools.com/java/")
+        ],
+        "youtube": [
+            ("Telusko", "https://www.youtube.com/@Telusko"),
+            ("Bro Code", "https://www.youtube.com/@BroCodez")
+        ]
+    },
+
+    "c++": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/c-plus-plus/"),
+            ("cplusplus.com", "https://cplusplus.com/doc/tutorial/")
+        ],
+        "youtube": [
+            ("CodeWithHarry", "https://www.youtube.com/@CodeWithHarry"),
+            ("Bro Code", "https://www.youtube.com/@BroCodez")
+        ]
+    },
+
+    "data structures": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/data-structures/"),
+            ("Programiz", "https://www.programiz.com/dsa")
+        ],
+        "youtube": [
+            ("Abdul Bari", "https://www.youtube.com/@abdul_bari"),
+            ("Jenny's Lectures", "https://www.youtube.com/@JennyslecturesCSIT")
+        ]
+    },
+
+    "algorithms": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/fundamentals-of-algorithms/"),
+            ("CP-Algorithms", "https://cp-algorithms.com/")
+        ],
+        "youtube": [
+            ("Abdul Bari", "https://www.youtube.com/@abdul_bari"),
+            ("William Fiset", "https://www.youtube.com/@WilliamFiset-videos")
+        ]
+    },
+
+    "object oriented programming": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/object-oriented-programming-oops-concept-in-java/"),
+            ("TutorialsPoint", "https://www.tutorialspoint.com/object_oriented_analysis_design/index.htm")
+        ],
+        "youtube": [
+            ("Telusko", "https://www.youtube.com/@Telusko"),
+            ("Bro Code", "https://www.youtube.com/@BroCodez")
+        ]
+    },
+
+    "problem solving": {
+        "websites": [
+            ("LeetCode", "https://leetcode.com/"),
+            ("HackerRank", "https://www.hackerrank.com/")
+        ],
+        "youtube": [
+            ("NeetCode", "https://www.youtube.com/@NeetCode"),
+            ("take U forward", "https://www.youtube.com/@takeUforward")
+        ]
+    },
+
+    # ================= Data & Analytics =================
+    "sql": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/sql-tutorial/"),
+            ("W3Schools", "https://www.w3schools.com/sql/")
+        ],
+        "youtube": [
+            ("Kudvenkat", "https://www.youtube.com/@kudvenkat"),
+            ("Simplilearn", "https://www.youtube.com/@SimplilearnOfficial")
+        ]
+    },
+
+    "excel": {
+        "websites": [
+            ("Microsoft Learn", "https://learn.microsoft.com/en-us/training/excel/"),
+            ("W3Schools", "https://www.w3schools.com/excel/")
+        ],
+        "youtube": [
+            ("Leila Gharani", "https://www.youtube.com/@LeilaGharani"),
+            ("ExcelIsFun", "https://www.youtube.com/@ExcelIsFun")
+        ]
+    },
+
+    "power bi": {
+        "websites": [
+            ("Microsoft Learn", "https://learn.microsoft.com/en-us/power-bi/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/power-bi/")
+        ],
+        "youtube": [
+            ("Guy in a Cube", "https://www.youtube.com/@GuyinaCube"),
+            ("Simplilearn", "https://www.youtube.com/@SimplilearnOfficial")
+        ]
+    },
+
+    "tableau": {
+        "websites": [
+            ("Tableau Official", "https://www.tableau.com/learn/training"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/tableau/")
+        ],
+        "youtube": [
+            ("Tableau Tim", "https://www.youtube.com/@TableauTim"),
+            ("Simplilearn", "https://www.youtube.com/@SimplilearnOfficial")
+        ]
+    },
+
+    "statistics": {
+        "websites": [
+            ("Khan Academy", "https://www.khanacademy.org/math/statistics-probability"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/statistics/")
+        ],
+        "youtube": [
+            ("StatQuest", "https://www.youtube.com/@statquest"),
+            ("Khan Academy", "https://www.youtube.com/@khanacademy")
+        ]
+    },
+
+    "data analysis": {
+        "websites": [
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/data-analysis/"),
+            ("Analytics Vidhya", "https://www.analyticsvidhya.com/")
+        ],
+        "youtube": [
+            ("Krish Naik", "https://www.youtube.com/@krishnaik06"),
+            ("Alex The Analyst", "https://www.youtube.com/@AlexTheAnalyst")
+        ]
+    },
+
+    "data visualization": {
+        "websites": [
+            ("Storytelling with Data", "https://www.storytellingwithdata.com/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/data-visualization/")
+        ],
+        "youtube": [
+            ("Tableau Tim", "https://www.youtube.com/@TableauTim"),
+            ("Simplilearn", "https://www.youtube.com/@SimplilearnOfficial")
+        ]
+    },
+
+    "pandas": {
+        "websites": [
+            ("Pandas Docs", "https://pandas.pydata.org/docs/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/python-pandas/")
+        ],
+        "youtube": [
+            ("Data School", "https://www.youtube.com/@DataSchool"),
+            ("Corey Schafer", "https://www.youtube.com/@coreyms")
+        ]
+    },
+
+    "numpy": {
+        "websites": [
+            ("NumPy Docs", "https://numpy.org/doc/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/python-numpy/")
+        ],
+        "youtube": [
+            ("Keith Galli", "https://www.youtube.com/@KeithGalli"),
+            ("Corey Schafer", "https://www.youtube.com/@coreyms")
+        ]
+    },
+
+    # ================= Web Development =================
+    "html": {
+        "websites": [
+            ("MDN", "https://developer.mozilla.org/en-US/docs/Web/HTML"),
+            ("W3Schools", "https://www.w3schools.com/html/")
+        ],
+        "youtube": [
+            ("Bro Code", "https://www.youtube.com/@BroCodez"),
+            ("Traversy Media", "https://www.youtube.com/@TraversyMedia")
+        ]
+    },
+
+    "css": {
+        "websites": [
+            ("MDN", "https://developer.mozilla.org/en-US/docs/Web/CSS"),
+            ("W3Schools", "https://www.w3schools.com/css/")
+        ],
+        "youtube": [
+            ("Kevin Powell", "https://www.youtube.com/@KevinPowell"),
+            ("Bro Code", "https://www.youtube.com/@BroCodez")
+        ]
+    },
+
+    "javascript": {
+        "websites": [
+            ("MDN", "https://developer.mozilla.org/en-US/docs/Web/JavaScript"),
+            ("W3Schools", "https://www.w3schools.com/js/")
+        ],
+        "youtube": [
+            ("Bro Code", "https://www.youtube.com/@BroCodez"),
+            ("Traversy Media", "https://www.youtube.com/@TraversyMedia")
+        ]
+    },
+
+    "react": {
+        "websites": [
+            ("React Docs", "https://react.dev/learn"),
+            ("W3Schools", "https://www.w3schools.com/react/")
+        ],
+        "youtube": [
+            ("Bro Code", "https://www.youtube.com/@BroCodez"),
+            ("Codevolution", "https://www.youtube.com/@Codevolution")
+        ]
+    },
+
+    "node": {
+        "websites": [
+            ("Node Docs", "https://nodejs.org/en/docs"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/nodejs/")
+        ],
+        "youtube": [
+            ("Traversy Media", "https://www.youtube.com/@TraversyMedia"),
+            ("Academind", "https://www.youtube.com/@Academind")
+        ]
+    },
+
+    "mongodb": {
+        "websites": [
+            ("MongoDB Docs", "https://www.mongodb.com/docs/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/mongodb/mongodb-tutorial/")
+        ],
+        "youtube": [
+            ("MongoDB", "https://www.youtube.com/@MongoDB"),
+            ("Traversy Media", "https://www.youtube.com/@TraversyMedia")
+        ]
+    },
+
+    # ================= Tools =================
+    "git": {
+        "websites": [
+            ("Git SCM", "https://git-scm.com/doc"),
+            ("Atlassian", "https://www.atlassian.com/git")
+        ],
+        "youtube": [
+            ("Traversy Media", "https://www.youtube.com/@TraversyMedia"),
+            ("Academind", "https://www.youtube.com/@Academind")
+        ]
+    },
+
+    # ================= Mobile Development =================
+    "android": {
+        "websites": [
+            ("Android Developers", "https://developer.android.com/docs"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/android/android-tutorial/")
+        ],
+        "youtube": [
+            ("Android Developers", "https://www.youtube.com/@AndroidDevelopers"),
+            ("Philipp Lackner", "https://www.youtube.com/@PhilippLackner")
+        ]
+    },
+
+    "kotlin": {
+        "websites": [
+            ("Kotlin Official Docs", "https://kotlinlang.org/docs/home.html"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/kotlin-programming-language/")
+        ],
+        "youtube": [
+            ("Philipp Lackner", "https://www.youtube.com/@PhilippLackner"),
+            ("Bro Code", "https://www.youtube.com/@BroCodez")
+        ]
+    },
+
+    "flutter": {
+        "websites": [
+            ("Flutter Official", "https://docs.flutter.dev/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/flutter/")
+        ],
+        "youtube": [
+            ("Flutter", "https://www.youtube.com/@flutterdev"),
+            ("CodeWithAndrea", "https://www.youtube.com/@CodeWithAndrea")
+        ]
+    },
+
+    "react native": {
+        "websites": [
+            ("React Native Docs", "https://reactnative.dev/docs/getting-started"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/react-native/")
+        ],
+        "youtube": [
+            ("Academind", "https://www.youtube.com/@Academind"),
+            ("Programming with Mash", "https://www.youtube.com/@ProgrammingWithMash")
+        ]
+    },
+
+    "api integration": {
+        "websites": [
+            ("Postman Learning", "https://learning.postman.com/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/blogs/what-is-api-integration/")
+        ],
+        "youtube": [
+            ("Traversy Media", "https://www.youtube.com/@TraversyMedia"),
+            ("Academind", "https://www.youtube.com/@Academind")
+        ]
+    },
+
+    # ================= UI / UX =================
+    "ui/ux": {
+        "websites": [
+            ("Interaction Design Foundation", "https://www.interaction-design.org/"),
+            ("NN Group", "https://www.nngroup.com/")
+        ],
+        "youtube": [
+            ("AJ&Smart", "https://www.youtube.com/@AJSmart"),
+            ("Flux Academy", "https://www.youtube.com/@FluxAcademy")
+        ]
+    },
+
+    "figma": {
+        "websites": [
+            ("Figma Learn", "https://www.figma.com/resources/learn-design/"),
+            ("UX Collective", "https://uxdesign.cc/")
+        ],
+        "youtube": [
+            ("Figma", "https://www.youtube.com/@Figma"),
+            ("DesignCourse", "https://www.youtube.com/@DesignCourse")
+        ]
+    },
+
+    "wireframing": {
+        "websites": [
+            ("UXPin", "https://www.uxpin.com/studio/blog/wireframing-guide/"),
+            ("Interaction Design Foundation", "https://www.interaction-design.org/")
+        ],
+        "youtube": [
+            ("AJ&Smart", "https://www.youtube.com/@AJSmart"),
+            ("CareerFoundry", "https://www.youtube.com/@CareerFoundry")
+        ]
+    },
+
+    "prototyping": {
+        "websites": [
+            ("Figma Prototyping", "https://help.figma.com/hc/en-us/articles/360040314193"),
+            ("UX Collective", "https://uxdesign.cc/")
+        ],
+        "youtube": [
+            ("DesignCourse", "https://www.youtube.com/@DesignCourse"),
+            ("Flux Academy", "https://www.youtube.com/@FluxAcademy")
+        ]
+    },
+
+    "design thinking": {
+        "websites": [
+            ("IDEO", "https://www.ideou.com/pages/design-thinking"),
+            ("Interaction Design Foundation", "https://www.interaction-design.org/")
+        ],
+        "youtube": [
+            ("AJ&Smart", "https://www.youtube.com/@AJSmart"),
+            ("Google Design", "https://www.youtube.com/@GoogleDesign")
+        ]
+    },
+
+    # ================= DevOps & Cloud =================
+    "linux": {
+        "websites": [
+            ("Linux Journey", "https://linuxjourney.com/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/linux-tutorial/")
+        ],
+        "youtube": [
+            ("Learn Linux TV", "https://www.youtube.com/@LearnLinuxTV"),
+            ("NetworkChuck", "https://www.youtube.com/@NetworkChuck")
+        ]
+    },
+
+    "docker": {
+        "websites": [
+            ("Docker Docs", "https://docs.docker.com/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/devops/docker-tutorial/")
+        ],
+        "youtube": [
+            ("TechWorld with Nana", "https://www.youtube.com/@TechWorldwithNana"),
+            ("NetworkChuck", "https://www.youtube.com/@NetworkChuck")
+        ]
+    },
+
+    "kubernetes": {
+        "websites": [
+            ("Kubernetes Docs", "https://kubernetes.io/docs/home/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/kubernetes/")
+        ],
+        "youtube": [
+            ("TechWorld with Nana", "https://www.youtube.com/@TechWorldwithNana"),
+            ("KodeKloud", "https://www.youtube.com/@KodeKloud")
+        ]
+    },
+
+    "aws": {
+        "websites": [
+            ("AWS Training", "https://aws.amazon.com/training/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/aws-tutorial/")
+        ],
+        "youtube": [
+            ("AWS", "https://www.youtube.com/@AmazonWebServices"),
+            ("Stephane Maarek", "https://www.youtube.com/@StephaneMaarek")
+        ]
+    },
+
+    "azure": {
+        "websites": [
+            ("Microsoft Learn", "https://learn.microsoft.com/en-us/azure/"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/microsoft-azure/")
+        ],
+        "youtube": [
+            ("Microsoft Azure", "https://www.youtube.com/@MicrosoftAzure"),
+            ("Adam Marczak", "https://www.youtube.com/@AdamMarczakYT")
+        ]
+    },
+
+    # ================= System Design =================
+    "system design": {
+        "websites": [
+            ("Grokking System Design", "https://www.educative.io/courses/grokking-the-system-design-interview"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/system-design-tutorial/")
+        ],
+        "youtube": [
+            ("ByteByteGo", "https://www.youtube.com/@ByteByteGo"),
+            ("Gaurav Sen", "https://www.youtube.com/@GauravSensei")
+        ]
+    },
+
+    # ================= ML / DL Frameworks =================
+    "tensorflow": {
+        "websites": [
+            ("TensorFlow Docs", "https://www.tensorflow.org/learn"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/tensorflow/")
+        ],
+        "youtube": [
+            ("TensorFlow", "https://www.youtube.com/@TensorFlow"),
+            ("Krish Naik", "https://www.youtube.com/@krishnaik06")
+        ]
+    },
+
+    "pytorch": {
+        "websites": [
+            ("PyTorch Docs", "https://pytorch.org/docs/stable/index.html"),
+            ("GeeksforGeeks", "https://www.geeksforgeeks.org/deep-learning/getting-started-with-pytorch/")
+        ],
+        "youtube": [
+            ("PyTorch", "https://www.youtube.com/@PyTorch"),
+            ("Aladdin Persson", "https://www.youtube.com/@AladdinPersson")
+        ]
+    }
+}
